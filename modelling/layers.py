debec3d97b3ce101d664c3a86d431b7541cac787
jackopenn <jackcpennington@gmail.com>
2025-11-21 14:25:17 +0000

    splash att fix

--- modelling/layers.py
+++ modelling/layers.py
@@ -26,12 +26,25 @@ class Attention(nnx.Module):
         self.v_proj = nnx.LinearGeneral(hidden_dim, (num_key_value_heads, head_dim), use_bias=use_bias, dtype=jnp.bfloat16, rngs=rngs)
         self.o_proj = nnx.LinearGeneral((num_attention_heads, head_dim), hidden_dim, axis=(-2, -1), use_bias=use_bias, dtype=jnp.bfloat16, rngs=rngs)
 
-    def _splash_attention_fn(self, seq_len: int):
-        return splash_attention.make_splash_mha(
-            mask=splash_attention.FullMask((seq_len, seq_len)),
-            head_shards=1,
-            q_seq_shards=1,
-        )
+    @jax.shard_map(
+        in_specs=(
+            P("data", None, None, None),
+            P("data", None, None, None),
+            P("data", None, None, None),
+        ),
+        out_specs=P("data", None, None, None),
+        check_vma=False
+    )
+    def _splash_attention_fn(self, q, k, v, seq_len: int):
+        return jax.vmap(
+            splash_attention.make_splash_mha(
+                mask=splash_attention.FullMask((seq_len, seq_len)),
+                head_shards=1,
+                q_seq_shards=1,
+            ),
+            in_axes=(0, 0, 0)
+        )(q, k, v)
+        
 
     def __call__(self, x):
         q, k, v = self.q_proj(x), self.k_proj(x), self.v_proj(x)
@@ -52,50 +65,10 @@ class Attention(nnx.Module):
                 q = jnp.swapaxes(q, 1, 2)
                 k = jnp.swapaxes(k, 1, 2)
                 v = jnp.swapaxes(v, 1, 2)
-            print(q.shape, k.shape, v.shape) # [batch_size, num_attention_heads, seq_len, head_dim]
+
             with jax.named_scope("attention"):
-            #     att = jax.shard_map(
-            #     partial(
-            #         flash_attention.flash_attention,
-            #         causal=True,
-            #         block_sizes=flash_attention.BlockSizes(
-            #             block_q=512,
-            #             block_k_major=1024,
-            #             block_k=512,
-            #             block_b=4,
+                att = self._splash_attention_fn(q, k, v, x.shape[1])
 
-            #             block_q_major_dkv=512,
-            #             block_k_major_dkv=1024,
-            #             block_k_dkv=512,
-            #             block_q_dkv=512,
-
-            #             block_k_major_dq=1024,
-            #             block_k_dq=512,
-            #             block_q_dq=512,
-            #         )
-            #     ),
-            #     in_specs=(
-            #         P("data", None, None, None),
-            #         P("data", None, None, None),
-            #         P("data", None, None, None),
-            #     ),
-            #     out_specs=P("data", None, None, None),
-            #     check_vma=False
-            # )(q, k, v)
-                att = jax.vmap(
-                        jax.shard_map(
-                            partial(self._splash_attention_fn, seq_len=x.shape[1]),
-                            in_specs=(
-                                P(None, None, None),
-                                P(None, None, None),
-                                P(None, None, None),
-                            ),
-                            out_specs=P(None, None, None),
-                            check_vma=False
-                        ),
-                        in_axes=(0, 0, 0)
-                    )(q, k, v)
-            
             att = jnp.swapaxes(att, 1, 2)
         else:
             att = jax.nn.dot_product_attention(query=q, key=k, value=v, is_causal=False)
