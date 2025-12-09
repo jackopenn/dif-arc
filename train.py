d7620463738d9183baa7b03c61b0b35b814ac690
jackopenn <jackcpennington@gmail.com>
2025-12-09 09:15:15 +0000

    debug print

--- train.py
+++ train.py
@@ -324,6 +324,7 @@ def main(cfg):
     t0 = time.perf_counter()
     for step, batch in enumerate(train_data_loader):
         batch = jax.tree.map(jnp.asarray, batch)
+        print(batch["x"].shape, batch["y"].shape, batch["aug_puzzle_idx"].shape)
         batch = shard_data(batch)
         if step == 0:
             carry = init_carry(batch, z_init, y_init)
